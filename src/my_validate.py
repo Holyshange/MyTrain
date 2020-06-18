3f5fcf56664c7329a57dccd8cb4f128d62e22246
Holyshange <Holyshange@192.168.248.1>
2020-06-18 18:11:01 +0800

    1

--- src/my_validate.py
+++ src/my_validate.py
@@ -3,7 +3,6 @@ import shutil
 import sys
 import cv2
 import time
-import imageio
 import numpy as np
 import tensorflow as tf
 from tensorflow.python import pywrap_tensorflow
@@ -25,17 +24,66 @@ def my_classifier(old_dir, new_dir, model_dir, image_width, image_height):
             for file in old_file_list:
                 if not (file.endswith('.png') | file.endswith('.bmp')):
                     continue
-                file_path = os.path.join(old_dir, file)
-                images = get_images(file_path, image_width, image_height)
+                image_path = os.path.join(old_dir, file)
+                images = get_images(image_path, image_height, image_width)
                 feed_dict = {images_placeholder: images, phase_train_placeholder: False}
                 num = sess.run(classifier, feed_dict=feed_dict)
                 dir_name = 'class_' + '%04d' % int(num)
                 new_dir_2 = os.path.join(new_dir, dir_name)
                 if not os.path.isdir(new_dir_2):
                     os.makedirs(new_dir_2)
-                new_file_path = os.path.join(new_dir_2, file)
-                print(new_file_path)
-                shutil.move(file_path, new_file_path)
+                new_image_path = os.path.join(new_dir_2, file)
+                print(new_dir_2)
+                shutil.move(image_path, new_image_path)
+
+def get_embeddings(image_path_list, model_dir, image_height, image_width):
+    with tf.Graph().as_default():
+        with tf.Session() as sess:
+            my_train.load_model(model_dir)
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            
+            for image_path in image_path_list:
+                images = get_images(image_path, image_height, image_width)
+                feed_dict = {images_placeholder: images, phase_train_placeholder: False}
+                emb = sess.run(embeddings, feed_dict=feed_dict)
+                print(emb)
+
+def get_images(image_path, image_height, image_width):
+    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+#     cv2.imshow('', image)
+#     cv2.waitKey(0)
+    image = cv2.resize(image, (image_width, image_height))
+#     cv2.imshow('', image)
+#     cv2.waitKey(0)
+    prewhitened = my_train.prewhiten_image(image)
+    images = np.stack([prewhitened])
+#     print(np.shape(images))
+    return images
+
+def get_trainable_variables(model_dir):
+    with tf.Graph().as_default():
+        with tf.Session() as sess:
+            my_train.load_model(model_dir)
+            variable_names = [v.name for v in tf.trainable_variables()]
+            with open('../data/trainable_variables.txt', 'w') as file:
+                values = sess.run(variable_names)
+                i=0
+                for k, v in zip(variable_names, values):
+                    i+=1
+                    if k.find('encode') != -1:
+                        file.write("Variable: ", k)
+                        file.write('\n')
+                        file.write("Shape: ", v.shape)
+                        file.write('\n')
+                        file.write(v)
+                        file.write('\n')
+                graph = tf.get_default_graph()
+                all_ops = graph.get_operations()
+                for el in all_ops:
+                    file.write(el.name)
+                    file.write('\n')
 
 # ================================================================================================
 
@@ -93,55 +141,6 @@ def validate_main(lfw_dir, pairs_txt, model_dir, image_height, image_width, batc
             accuracy_, threshold_ = my_train.get_accuracy_and_threshold(emb_array, issame_list)
             print("Accuracy: %2.3f\nThreshold: %2.3f" % (np.mean(accuracy_), threshold_))
 
-def get_variables(image_path_list, model_dir, image_width, image_height):
-#     image_path_array = np.array(image_path_list)
-    with tf.Graph().as_default():
-        with tf.Session() as sess:
-            my_train.load_model(model_dir)
-            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
-            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-#             embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
-            # prelogits = tf.get_default_graph().get_tensor_by_name("prelogits:0")
-            logits = tf.get_default_graph().get_tensor_by_name("Logits/BiasAdd:0")
-            classifier = tf.argmax(logits, 1)[0]
-            
-            for image_path in image_path_list:
-                images = get_images(image_path, image_width, image_height)
-                feed_dict = {images_placeholder: images, phase_train_placeholder: False}
-                print(sess.run(classifier, feed_dict=feed_dict))
-                
-
-def get_variable_names(checkpoint_path, file_name):
-    reader=pywrap_tensorflow.NewCheckpointReader(checkpoint_path)
-    var_to_shape_map=reader.get_variable_to_shape_map()
-    with open(file_name, 'w') as file:
-        for key in var_to_shape_map:
-            file.write(key)
-            file.write('\n')
-
-def get_all_nodes(model_dir):
-    with tf.Graph().as_default():
-        with tf.Session() as sess:
-            my_train.load_model(model_dir)
-            variable_names = [v.name for v in tf.trainable_variables()]
-            with open('../data/variables.txt', 'w') as file:
-                values = sess.run(variable_names)
-                i=0
-                for k, v in zip(variable_names, values):
-                    i+=1
-                    if k.find('encode')!=-1:
-                        file.write("Variable: ", k)
-                        file.write('\n')
-                        file.write("Shape: ", v.shape)
-                        file.write('\n')
-                        file.write(v)
-                        file.write('\n')
-                graph = tf.get_default_graph()
-                all_ops = graph.get_operations()
-                for el in all_ops:
-                    file.write(el.name)
-                    file.write('\n')
-
 def run_validate():
     lfw_dir = '../data/lfw_mtcnn_224'
     pairs_txt = '../data/pairs.txt'
@@ -153,35 +152,17 @@ def run_validate():
     validate_main(lfw_dir, pairs_txt, model_dir, image_height, image_width, 
                   batch_size, gpu_memory_fraction)
 
-def get_images(image_path, image_height, image_width):
-    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-#     cv2.imshow('', image)
-#     cv2.waitKey(0)
-    image = cv2.resize(image, (image_width, image_height))
-#     cv2.imshow('', image)
-#     cv2.waitKey(0)
-    prewhitened = my_train.prewhiten_image(image)
-    images = np.stack([prewhitened])
-#     print(np.shape(images))
-    return images
+# ================================================================================================
 
-def test_2():
+def run_test():
+    model_dir = '../models/Inception_resnet_v1_20170512110547'
     old_dir = '../data/old_dir'
     new_dir = '../data/new_dir'
-    model_dir = '../models/Inception_resnet_v1_20170512110547'
     my_classifier(old_dir, new_dir, model_dir, 160, 160)
-    
-def test_1():
-    checkpoint_path = '../models/Inception_resnet_v1_20170512110547/model-20170512-110547.ckpt-250000'
-    file_name = '../data/Inception_resnet_v1_variables.txt'
-    get_variable_names(checkpoint_path, file_name)
-    None
 
 if __name__ == '__main__':
 #     run_validate()
-    test_2()
-#     model_dir = '../models/Inception_resnet_v1_20170512110547'
-#     show_all_nodes(model_dir)
+    run_test()
     print('____End____')
 
 
